1fd449f4dfb6e0fe3cea719305f96ef978e87836
russianwaifu <72613783+MrYapikZ@users.noreply.github.com>
2026-04-16 17:08:00 +0700

    feat: main page no animation and projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,63 @@
+'use client';
 import Image from "next/image";
+import { DiscordLogoIcon, FacebookLogoIcon, GithubLogoIcon, InstagramLogoIcon } from "@phosphor-icons/react";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <>
+      <section className="p-4 h-screen w-screen overflow-hidden bg-white">
+        <div className="relative w-full h-full overflow-hidden">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
+          src="/background/wallhaven-1qd9o1.png"
+          alt="background"
+          fill
           priority
+          className="object-cover"
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+        <div className="absolute inset-0 bg-white/0"></div>
+
+        <div className="relative w-full h-full border-4 border-black grid grid-cols-12 grid-rows-6">
+          {/* Nav */}
+          <div className="bg-white border-r-4 border-black row-start-1 row-end-7 flex items-center justify-center">
+            <p className="font-sans font-bold text-8xl [writing-mode:vertical-rl] [text-orientation:mixed] rotate-180">
+              STUPID GENERALIST
+            </p>
+          </div>
+          {/* Name */}
+          <div className="bg-white border-b-4 border-l-4 border-black col-start-9 col-end-13 flex flex-col items-center justify-center">
+            <h1 className="font-heading text-[12rem]">
+              Dexkhead
+            </h1>
+          </div>
+          {/* Projects */}
+          <div className="bg-white border-b-4 border-l-4 border-black col-start-9 col-end-13 row-start-2 row-end-4">
+
+          </div>
+          {/* Bio */}
+          <div className="bg-white border-l-4 border-black col-start-9 col-end-13 row-start-4 row-end-7 p-8 flex flex-col items-center justify-center">
+            <p className="font-sans text-xl">
+              I’m a <b>Technical Director</b> in the animation industry, currently exploring Software Engineering, Data Science, and Game Development to understand how technology can enhance storytelling and design.
+            </p>
+          </div>
+          {/* Contact */}
+          <div className="bg-white border-l-4 border-t-4 border-black col-start-4 col-end-9 row-start-5 row-end-7 flex flex-col items-center justify-center gap-4">
+            <a className="text-xl" href="mailto:yapi@expiproject.com">yapi@expiproject.com</a>
+            <div className="flex flex-row gap-8">
+              <InstagramLogoIcon size={32} />
+              <FacebookLogoIcon size={32} />
+              <DiscordLogoIcon size={32} />
+              <GithubLogoIcon size={32} />
+            </div>
+          </div>
+          {/* Japanese Name */}
+          <div className="col-start-2 col-end-4 row-start-3 row-end-7 flex flex-col items-center justify-end">
+            <h1 className="font-japanese text-[12rem] text-white p-8 px-14 align-end [writing-mode:vertical-rl] [text-orientation:upright] [-webkit-text-stroke:4px_black]">
+              甲板頭
+            </h1>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+      </div>
+    </section >
+    </>
   );
 }
